9e80f47324d4f2b5d1ff17c02e62b0ef96cbceb2
Ioan-Paul <ioanpaulpirau@gmail.com>
2016-07-03 19:33:25 +0300

    -cleaned up an rearanged code

--- mqtt_esp8266/mqtt_esp8266.ino
+++ mqtt_esp8266/mqtt_esp8266.ino
@@ -19,21 +19,17 @@
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
-#define DEBUG 1
-
 /************************* WiFi Access Point *********************************/
 
 #define WLAN_SSID       "JPHomeCJ_WiFiGuest"
-#define WLAN_PASS       ""
+#define WLAN_PASS       ":-)"
 
 /************************* Adafruit.io Setup *********************************/
 
-#define AIO_SERVER      "jppcj.go.ro"
-//#define AIO_SERVER      "broker.mqtt-dashboard.com"
-//#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    "...your AIO username (see https://accounts.adafruit.com)..."
-#define AIO_KEY         "...your AIO key..."
+#define SERVER       "jppcj.go.ro"
+#define SERVER_PORT  1883                   // use 8883 for SSL
+#define DEVICE_UID   "proto"                // device unique identifier
+#define DEVICE_KEY   ""
 
 /************ Global State (you don't need to change this!) ******************/
 
@@ -42,32 +38,28 @@ WiFiClient client;
 // or... use WiFiFlientSecure for SSL
 //WiFiClientSecure client;
 
-// Store the MQTT server, username, and password in flash memory.
-// This is required for using the Adafruit MQTT library.
-const char MQTT_SERVER[] PROGMEM    = AIO_SERVER;
-const char MQTT_USERNAME[] PROGMEM  = AIO_USERNAME;
-const char MQTT_PASSWORD[] PROGMEM  = AIO_KEY;
+// Store the MQTT server, username, and password in flash memory. This is required for using the Adafruit MQTT library.
+const char MQTT_SERVER[] PROGMEM    = SERVER;
+const char MQTT_USERNAME[] PROGMEM  = DEVICE_UID;
+const char MQTT_PASSWORD[] PROGMEM  = DEVICE_KEY;
 
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, AIO_SERVERPORT, MQTT_USERNAME, MQTT_PASSWORD);
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, SERVER_PORT, MQTT_USERNAME, MQTT_PASSWORD);
 
 /****************************** Feeds ***************************************/
 
-// Setup a feed called 'photocell' for publishing.
-// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-//const char PHOTOCELL_FEED[] PROGMEM = AIO_USERNAME "/feeds/photocell";
-const char PHOTOCELL_FEED[] PROGMEM = AIO_USERNAME "test";
-Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt, PHOTOCELL_FEED);
+// Notice MQTT paths follow the form: <username>/feeds/<feedname>
+
+// Setup a feed called 'SPEPrototype' for publishing.
+const char TEST_PUBLISH_FEED[] PROGMEM = DEVICE_UID "/feeds/test";
+Adafruit_MQTT_Publish publisher = Adafruit_MQTT_Publish(&mqtt, TEST_PUBLISH_FEED);
 
 // Setup a feed called 'onoff' for subscribing to changes.
-const char ONOFF_FEED[] PROGMEM = AIO_USERNAME "/feeds/onoff";
-Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, ONOFF_FEED);
+const char TEST_SUBSCRIBE_FEED[] PROGMEM = DEVICE_UID "/feeds/onoff";
+Adafruit_MQTT_Subscribe subscriber = Adafruit_MQTT_Subscribe(&mqtt, TEST_SUBSCRIBE_FEED);
 
-/*************************** Sketch Code ************************************/
+/*************************** Helper Functions ************************************/
 
-// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
-// for some reason (only affects ESP8266, likely an arduino-builder bug).
-//void MQTT_connect();
 // Function to connect and reconnect as necessary to the MQTT server.
 // Should be called in the loop function and it will take care if connecting.
 void MQTT_connect() {
@@ -95,6 +87,41 @@ void MQTT_connect() {
   Serial.println("MQTT Connected!");
 }
 
+// Get the time from a ping, See more at: http://www.esp8266.com/viewtopic.php?f=29&t=6007&start=5#sthash.zBe2qWFY.dpuf
+String getTime() {
+  WiFiClient client;
+  while (!!!client.connect("google.com", 80)) {
+    Serial.println("connection failed, retrying...");
+  }
+
+  client.print("HEAD / HTTP/1.1\r\n\r\n");
+ 
+  while(!!!client.available()) {
+     yield();
+  }
+
+  while(client.available()){
+    if (client.read() == '\n') {    
+      if (client.read() == 'D') {    
+        if (client.read() == 'a') {    
+          if (client.read() == 't') {    
+            if (client.read() == 'e') {    
+              if (client.read() == ':') {    
+                client.read();
+                String theDate = client.readStringUntil('\r');
+                client.stop();
+                return theDate;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+} 
+
+/*************************** Sketch Setup and Looper Code ************************************/
+
 void setup() {
   Serial.begin(115200);
   delay(10);
@@ -117,13 +144,12 @@ void setup() {
   Serial.println("IP address: "); Serial.println(WiFi.localIP());
 
   // Setup MQTT subscription for onoff feed.
-  mqtt.subscribe(&onoffbutton);
+  mqtt.subscribe(&subscriber);
 }
 
 uint32_t x=0;
-
-
-Adafruit_MQTT_Publish publisher = Adafruit_MQTT_Publish(&mqtt, "test");
+char text_to_send[100];
+char converter[10];
 
 void loop() {
   // Ensure the connection to the MQTT server is alive (this will make the first
@@ -133,22 +159,25 @@ void loop() {
 
   // this is our 'wait for incoming subscription packets' busy subloop
   // try to spend your time here
-/*
   Adafruit_MQTT_Subscribe *subscription;
   while ((subscription = mqtt.readSubscription(5000))) {
-    if (subscription == &onoffbutton) {
+    if (subscription == &subscriber) {
       Serial.print(F("Got: "));
-      Serial.println((char *)onoffbutton.lastread);
+      Serial.println((char *)subscriber.lastread);
     }
   }
-  */
+
+  strcpy(text_to_send, getTime().c_str());
+  strcat(text_to_send, " - NodeMCU says hello for the ");
+  strcat(text_to_send, itoa(x++, converter, 10));
+  strcat(text_to_send, "'th time :)");
 
   // Now we can publish stuff!
-  Serial.print(F("\nSending photocell val "));
-  Serial.print(x);
+  Serial.print(F("\nSending :"));
+  Serial.print(text_to_send);
   Serial.print("...");
 //  if (! photocell.publish(x++)) {
-  if (! publisher.publish(x++)) {
+  if (! publisher.publish(text_to_send)) {
     Serial.println(F("Failed"));
   } else {
     Serial.println(F("OK!"));
